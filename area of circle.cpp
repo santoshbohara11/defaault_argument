6c6c1e472f5f9d227ab654ba0af579c413e417d4
santoshbohara11 <santoshbohara135790@gmail.com>
2023-09-08 20:53:04 +0545

    second commit

---
+++ area of circle.cpp
@@ -0,0 +1,15 @@
+//WAP to calculate the area of circle 
+#include<iostream>
+using namespace std;
+
+void circle(int r=7)
+{
+	float a=0;
+	a=3.14*r*r;
+	cout<<"area of circle ="<<a;
+}
+int main()
+{
+	circle(3);
+	return 0;
+}
---
+++ rectangle.cpp
@@ -0,0 +1,15 @@
+//WAP to calculate the area of rectangle using deafult argument
+#include<iostream>
+using namespace std;
+
+void area(float l=9,float b=8.6)
+{
+	float a;
+	a=(l*b);
+	cout<<"area of circle ="<<a;
+}
+int main()
+{
+	area(4.5,2.3);
+	return 0;
+}
